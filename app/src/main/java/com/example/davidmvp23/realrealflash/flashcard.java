d8427b662316e076115dcb871c9b388520b00159
Tim <tng@jhu.edu>
2015-03-02 23:48:01 -0500

    Get flashcard.java file

--- app/src/main/java/com/example/davidmvp23/realrealflash/flashcard.java
+++ app/src/main/java/com/example/davidmvp23/realrealflash/flashcard.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class flashcard extends Activity {
 
+    //This is test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
